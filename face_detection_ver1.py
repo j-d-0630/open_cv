e18efb4e081c5dff17ee8c5ac36f2de60dea0b60
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-08-27 01:28:57 +0900

    Update face_detection_ver1.py
    <変更点>
    ・顔検出時は未検出であることをAWSIOTへ送る処理を追加
    ・顔検出の周期を3秒に変更
    ・AWSIOTへの通知内容を現在時刻と検知結果(0：未検知 , 1：検知)に変更

--- face_detection_ver1.py
+++ face_detection_ver1.py
@@ -1,25 +1,27 @@
 """
 顔検出アプリ
+
+<変更点>
+・顔検出時は未検出であることをAWSIOTへ送る処理を追加
+・顔検出の周期を3秒に変更
+・AWSIOTへの通知内容を現在時刻と検知結果(0：未検知 , 1：検知)に変更
 """
 from awscrt import io, mqtt, auth, http
 from awsiot import mqtt_connection_builder
 import time as t
+import datetime
 import json
 
 import os
 import cv2
 import sys
 
-os.system("pwd")
-print(sys.base_prefix)
-print(cv2)
-
 # Define ENDPOINT, CLIENT_ID, PATH_TO_CERT, PATH_TO_KEY, PATH_TO_ROOT, MESSAGE, TOPIC, and RANGE
-ENDPOINT = ""
+ENDPOINT = ".amazonaws.com"
 CLIENT_ID = "testDevice"
 PATH_TO_CERT = "./cert/-certificate.pem.crt"
 PATH_TO_KEY = "./cert/-private.pem.key"
-PATH_TO_ROOT = "./cert/.pem"
+PATH_TO_ROOT = "./cert/AmazonRootCA1.pem"
 MESSAGE = "Hello World"
 TOPIC = "face_detection/testing"
 
@@ -59,48 +61,59 @@ mouth_cascade = cv2.CascadeClassifier(mouth_cascade_path)
 # VideoCaptureオブジェクト取得
 capture = cv2.VideoCapture(0) # 引数は接続されてるカメラの番号
 
-# lena = cv2.imread("lena.png")
 while 1:
-    ret, frame = capture.read()
-    # frame = lena
-    frame_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(frame_g)
-    cv2.putText(frame, 'look at a camera!', (10,30), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), thickness=2)
+  detect_flg = 0 # 検出の有無 (未検出なら0,検出したら1)
+  ret, frame = capture.read()
 
-    # 【AI】顔の中に目（と口）がある場合に人と認識するに改良する
-    for x, y, w, h in faces:
-        # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = frame[y: y + h, x: x + w]
-        face1 = frame[y: y + h//2 - 0, x: x + w] # 目を探す範囲を顔の上半分に限定
-        face2 = frame[y + h//2 + 35: y + h, x: x + w] # 口を探す範囲を顔の下半分に限定
-        face_gray1 = frame_g[y: y + h//2 - 0, x: x + w]
-        face_gray2 = frame_g[y + h//2 + 35: y + h, x: x + w]
-        eyes = eye_cascade.detectMultiScale(face_gray1)
-        # print("顔ある")
-        for (ex, ey, ew, eh) in eyes:
-            # print("目もある")
-            mouths = mouth_cascade.detectMultiScale(face_gray2)
-            for (mx, my, mw, mh) in mouths:
-                cv2.putText(face2, 'Not masked!!', (mx, my), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), thickness=2)
-                # print("口もある")
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                cv2.rectangle(face1, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-                cv2.rectangle(face2, (mx, my), (mx + mw, my + mh), (255, 255, 255), 2)
-                
-                #Send to AWS IOR CORE
-                data = "{}".format('Face detect!!')
-                message = {"message" : data}
-                mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
-                print("Published: '" + json.dumps(message) + "' to the topic: " + "'face_detection/testing'")
-                t.sleep(0.1)
+  frame_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces = face_cascade.detectMultiScale(frame_g)
+  cv2.putText(frame, 'look at a camera!', (10,30), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), thickness=2)
 
+  for x, y, w, h in faces:
+    face = frame[y: y + h, x: x + w]
+    face1 = frame[y: y + h//2 - 0, x: x + w] # 目を探す範囲を顔の上半分に限定
+    face2 = frame[y + h//2 + 35: y + h, x: x + w] # 口を探す範囲を顔の下半分に限定
+    face_gray1 = frame_g[y: y + h//2 - 0, x: x + w]
+    face_gray2 = frame_g[y + h//2 + 35: y + h, x: x + w]
+    eyes = eye_cascade.detectMultiScale(face_gray1)
 
-    cv2.imshow("image", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"): #waitkeyの引数はキーボードからの入力待ち時間。この時間で再度read()されるまでの時間を調整できる
-        print('Publish End')
-        disconnect_future = mqtt_connection.disconnect()
-        disconnect_future.result()
-        break
+    for (ex, ey, ew, eh) in eyes:
+      mouths = mouth_cascade.detectMultiScale(face_gray2)
+      for (mx, my, mw, mh) in mouths:
+        detect_flg = 1 # 顔検出したのでフラグを1に設定
+        cv2.putText(face2, 'Not masked!!', (mx, my), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), thickness=2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.rectangle(face1, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        cv2.rectangle(face2, (mx, my), (mx + mw, my + mh), (255, 255, 255), 2)
+
+        #Send to AWS IOR CORE
+        date = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))))
+        result = detect_flg
+        message = {"detection_result" : [date,result]} # {日本時刻,顔検出結果}を格納
+        mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+        print("Published: '" + json.dumps(message) + "' to the topic: " + "'face_detection/testing'")
+        t.sleep(0.1)
+  
+  #顔検出しなかった場合の処理
+  if detect_flg == 0:
+    #Send to AWS IOR CORE
+    date = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))))
+    result = detect_flg
+    message = {"detection_result" : [date,result]} # {日本時刻,顔検出結果}を格納
+    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + "'face_detection/testing'")
+    t.sleep(0.1)
+
+  # 描画
+  cv2.imshow("image", frame)
+  if cv2.waitKey(1) & 0xFF == ord("q"): #waitkeyの引数はキーボードからの入力待ち時間。この時間で再度read()されるまでの時間を調整できる
+      print('Publish End')
+      disconnect_future = mqtt_connection.disconnect()
+      disconnect_future.result()
+      break
+  
+  # 3秒ごとに顔検出実施
+  t.sleep(2.9)
 
 capture.release()
 cv2.destroyAllWindows()
