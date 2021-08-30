d436c494648797328c96a48c52a0f9f0bee2efdc
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-08-30 23:05:52 +0900

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,18 +1,21 @@
-# ①ライブラリのimport
+import json
 import boto3
-from datetime import datetime
- 
-print('Loading function')      # ②Functionのロードをログに出力
- 
-s3 = boto3.resource('s3')      # ③S3オブジェクトを取得
- 
-# ④Lambdaのメイン関数
+
+s3 = boto3.resource("s3")
+
 def lambda_handler(event, context):
+    # TODO implement
+    print(event["time"])
+    print(event["detection_result"])
     
-    bucket = 'your_backet_name'    # ⑤バケット名を指定
-    key = 'test_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.txt'  # ⑥オブジェクトのキー情報を指定
-    file_contents = 'Lambda test'  # ⑦ファイルの内容
+    bucket = "for-face-detection-1"
+    key = "data/" + "detection_result" + ".txt"
+    file_contents = event["time"]
+
+    obj = s3.Object(bucket,key)
+    obj.put(Body=file_contents)
     
-    obj = s3.Object(bucket,key)     # ⑧バケット名とパスを指定
-    obj.put( Body=file_contents )   # ⑨バケットにファイルを出力
-    return
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
